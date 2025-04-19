65c3d0e59e086f7591f2149fed7a21d18ad6f140
Gladimir Ceroni Catarino <gladimir@gmail.com>
2025-04-19 13:12:04 -0300

    Update gerar-arvore.mjs

--- gerar-arvore.mjs
+++ gerar-arvore.mjs
@@ -2,26 +2,26 @@
 import fs from 'fs';
 import { globby } from 'globby';
 
-const entries = await globby(
-  ['**/*', '!**/node_modules/**', '!**/.git/**', '!files.json', '!.github/**'],
-  { onlyFiles: false }
-);
+const entries = await globby(['**/*', '!**/node_modules/**', '!**/.git/**', '!files.json', '!.github/**'], {
+  onlyFiles: false
+});
 
 const pastas = new Set();
 const arquivos = [];
 
-entries.forEach((entry) => {
+for (const entry of entries) {
   const stat = fs.statSync(entry);
+
   if (stat.isDirectory()) {
     pastas.add(entry);
   } else if (stat.isFile()) {
     arquivos.push(entry);
   }
-});
+}
 
 const estrutura = {
   pastas: Array.from(pastas).sort(),
-  arquivos: arquivos.sort(),
+  arquivos: arquivos.sort()
 };
 
 fs.writeFileSync('files.json', JSON.stringify(estrutura, null, 2));
